5030051584ffe8106de9177f516afe9436736e18
matlavv <mathis.laversin92@gmail.com>
2024-04-24 12:05:53 +0200

    responsive on big screen

--- app/layout.js
+++ app/layout.js
@@ -3,5 +3,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  return <div className={inter.className}>{children}</div>;
+  return <div>{children}</div>;
 }
