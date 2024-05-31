4352e96c9394c5b284362aa839ce5b80fb7d76c5
matlavv <mathis.laversin92@gmail.com>
2024-05-31 12:17:07 +0200

    add section on home page

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <AppPresentation />
         <GoToAction />
         <Rewards />
+        <HowItWorks />
         <EcologicalLearning />
         <JoinNewsletter />
       </div>
